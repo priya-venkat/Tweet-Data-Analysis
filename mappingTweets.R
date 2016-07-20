a0ce3440eedeee39de21d5eba0a3a024ff9f5cd7
prya26 <priya26@tamu.edu>
2016-07-20 08:45:54 -0500

    Create mappingTweets.R

---
+++ mappingTweets.R
@@ -0,0 +1,40 @@
+######### superimpose tweet locations on map of places   ##########
+
+latt = read.csv("/Users/Neo/Documents/LearnPython/poke_latt.csv")
+long = read.csv("/Users/Neo/Documents/LearnPython/poke_long.csv")
+
+coord = cbind(long,latt)
+colnames(coord) = c("Longitude","Latitude")
+
+#install.packages("maps")
+#install.packages("ggmap")
+library(maps)
+library(ggmap)
+
+chicago = get_map(location = "chicago", zoom = 10)
+california = get_map(location = "california", zoom = 7)
+usa = get_map(location = "USA", zoom = 4)
+
+# ggmap(chicago)
+# ggmap(california)
+
+# Plot the first 100 motor vehicle thefts:
+ggmap(chicago) + geom_point(data = coord, aes(x = Longitude, y = Latitude))
+ggmap(california) + geom_point(data = coord, aes(x = Longitude, y = Latitude))
+ggmap(usa) + geom_point(data = coord, aes(x = Longitude, y = Latitude), color="red")
+
+# Round our latitude and longitude to 1 digit of accuracy, and create a crime counts data frame for each area:
+LatLonCounts = as.data.frame(table(round(coord$Longitude,1), round(coord$Latitude,1)))
+
+#str(LatLonCounts)
+
+LatLonCounts$Long = as.numeric(as.character(LatLonCounts$Var1))
+LatLonCounts$Lat = as.numeric(as.character(LatLonCounts$Var2))
+
+# Plot these points on our map:
+# Change the color scheme:
+ggmap(usa) + geom_point(data = LatLonCounts, aes(x = Long, y = Lat, color = Freq, size=Freq)) + scale_colour_gradient(low="yellow", high="red")
+
+ggmap(usa) + geom_tile(data = LatLonCounts, aes(x = Long, y = Lat, alpha = Freq), fill="red")
+
+ggmap(california) + geom_tile(data = LatLonCounts, aes(x = Long, y = Lat, alpha = Freq), fill="red")
